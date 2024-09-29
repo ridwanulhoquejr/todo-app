60f01e4eca141b3d96a0665a2e64a7adbbd13d5c
ridwanulhoquejr <ridwanul.hoque.jr@gmail.com>
2024-09-29 16:13:53 +0600

    dockerized the app for both database and api

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Starting the application...")
+	fmt.Fprintf(w, "Hello world!\n")
+}
+
 func main() {
 
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ridwanulhoquejr/todo-app
+
+go 1.23.1
